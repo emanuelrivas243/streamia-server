0c15664d24f4c60ae1fd273002d315356d44232e
Sara <sarareyes1227@gmail.com>
2025-10-10 19:56:31 -0500

    Creating CRUD for users

---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Servidor funcionando en TypeScript!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
